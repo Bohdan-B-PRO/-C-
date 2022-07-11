08646ec3f3f213eff341dd88c958098e76eb7764
BohdanBoss <bogdanburduluk@gmail.com>
2022-07-11 19:25:46 +0300

    Added Example005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввидите имя пользовотеля ");
+string username = Console.ReadLine();
+
+if(username.ToLower=="маша")
+{
+    Console.Write("Ура это же МАША! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
