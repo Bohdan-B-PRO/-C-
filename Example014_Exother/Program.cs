17064f4d1023bc2a4573a68c315d0af3135bdc16
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-15 21:59:08 +0300

    Added info Example 014

--- Example014_Exother/Program.cs
+++ Example014_Exother/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿/*
+using System.Linq;
 var a = 12;
 
 //Console.WriteLine(a.GetType().Name);
@@ -7,3 +8,23 @@ var datd = new int[]{1,2,3,4}
             .Where(e => e > 0)
             .Select(e => new{q = e, w = e=1});
 Console.WriteLine(data.GetType().Name);
+*/
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .Select(item => item.Split(","))
+                .Select(e =>(x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 == 0 )
+                .Select(point => (point.x * 10, point.y + 10))
+                .ToArray();
+    for(int i = 0; i < data.Length; i++)
+    {
+       Console.WriteLine(data[i]);
+    }
