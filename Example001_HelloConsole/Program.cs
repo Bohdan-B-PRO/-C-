43a464068963a37716311286bfa84090fd777767
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-19 10:04:24 +0300

    Added info Example 001

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine(" 2 Hello, World! ");
+﻿//Console.WriteLine(" 2 Hello, World! ");
+/*
+String path = "C:Users,user,Desktop,C#,Example001_HelloConsole";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+*/
+
+void CatalogInfo(string path, string indent ="")
+{
+DirectoryInfo catalog = new DirectoryInfo(path);
+
+DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for(int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+}
+string path = @"PS C:\Users\user\Desktop\C#\Example001_HelloConsole>"; 
+CatalogInfo(path);
