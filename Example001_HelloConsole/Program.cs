5fa833b0aa8268203369beda1f6b028744ff85d4
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-20 13:08:08 +0300

    Added info Example 001

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -10,6 +10,7 @@ for (int i = 0; i < fi.Length; i++)
     System.Console.WriteLine(fi[i].Name);
 }
 */
+/*
 
 void CatalogInfo(string path, string indent ="")
 {
@@ -31,3 +32,15 @@ DirectoryInfo[] catalogs = catalog.GetDirectories();
 }
 string path = @"PS C:\Users\user\Desktop\C#\Example001_HelloConsole>"; 
 CatalogInfo(path);
+*/
+void Towers(string with ="1", string on = "3", string some = "2", int count = 5 )
+{
+    if (count > 1) Towers(with, some, on, count -1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+
+Towers();
+
+
