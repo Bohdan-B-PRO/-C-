0352c5999650131c7f4694e370b1eca7856ded13
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-20 23:46:58 +0300

    Added info Example 001

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -33,6 +33,7 @@ DirectoryInfo[] catalogs = catalog.GetDirectories();
 string path = @"PS C:\Users\user\Desktop\C#\Example001_HelloConsole>"; 
 CatalogInfo(path);
 */
+/*
 void Towers(string with ="1", string on = "3", string some = "2", int count = 5 )
 {
     if (count > 1) Towers(with, some, on, count -1);
@@ -42,5 +43,76 @@ void Towers(string with ="1", string on = "3", string some = "2", int count = 5 
 
 
 Towers();
+*/
+/*
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+//                  0   1    2     3    4    5    6    7    8    9    10    11
+void InOrderTraversal(int pos = 1)
+{
+    if(pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int  right = 2 * pos + 1;
+        if(left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if(right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
+*/
+
+
+decimal fRec = 0;
+decimal fIte = 0;
+
+decimal FiconacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FiconacciRecursion(n - 1) + FiconacciRecursion(n - 2);
+}
+
+decimal FibonacciIteration(int n)
+{
+    fIte++; 
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for(int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+
+//# 1 2 3 4 5 6
+//V 1 1 2 3 5 8
+
+Console.ReadLine();
+DateTime dt = DateTime.Now;
+for(int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} FIte = {fIte.ToString("### ### ###")}, -15");
+    fIte = 0;
+}
+System.Console.WriteLine(+(DateTime.Now - dt).TotalMilliseconds);
+
+
+Console.WriteLine();
+Console.ReadLine();
+dt = DateTime.Now;
+for(int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} FRec = {fRec.ToString("### ### ###")}, -15");
+    fRec = 0;
+}
+System.Console.WriteLine(+(DateTime.Now - dt).TotalMilliseconds);
+
+Console.ReadLine();
+
 
 
